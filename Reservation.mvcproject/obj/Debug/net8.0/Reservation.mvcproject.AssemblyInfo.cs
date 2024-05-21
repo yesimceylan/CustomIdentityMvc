775e90a45dd8dbf3ca2ea3b0e9440af12c027195
yesimceylan <yesimceylan7@icloud.com>
2024-05-21 15:07:52 +0300

    rezDate from today , rezEndDate from tomorrow initiated in CreateReservation

--- Reservation.mvcproject/obj/Debug/net8.0/Reservation.mvcproject.AssemblyInfo.cs
+++ Reservation.mvcproject/obj/Debug/net8.0/Reservation.mvcproject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Reservation.mvcproject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cda55fd8071960cd9466b3fec87e6c85ca10b864")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e4c57cb2a1b431fd01a0eb6a99a7f6fe816b0868")]
 [assembly: System.Reflection.AssemblyProductAttribute("Reservation.mvcproject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Reservation.mvcproject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
